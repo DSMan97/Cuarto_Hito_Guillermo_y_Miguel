825d00f29bbb78f99d1def778dc848603f88761d
GUILLERMO CRESPO AGUAYO <guillermo.crespo@utmac0012.local>
2018-05-18 13:50:46 +0200

    reseteando repo

--- Cuarto_Hito/Pods/Target Support Files/Pods-Cuarto_HitoTests/Pods-Cuarto_HitoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cuarto_HitoTests : NSObject
-@end
-@implementation PodsDummy_Pods_Cuarto_HitoTests
-@end
--- Cuarto_Hito/Pods/Target Support Files/Pods-Cuarto_HitoUITests/Pods-Cuarto_HitoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cuarto_HitoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Cuarto_HitoUITests
-@end
